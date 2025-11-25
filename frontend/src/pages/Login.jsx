8eacc58cef6a02523b48e965dde549d97fbe0da5
subhranta_703 <subhrantanayak6535@gmail.com>
2025-11-25 13:11:12 +0530

    login image changed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import  api  from "../api/api";
 import { useNavigate, Link } from "react-router-dom";
 import { FaGoogle, FaLinkedinIn, FaPhoneAlt } from "react-icons/fa";
-import loginjp from "../assets/Login.png";
+import loginjp from "../assets/login.png";
 import { useGoogleLogin } from '@react-oauth/google';
 
 export default function Login() {
